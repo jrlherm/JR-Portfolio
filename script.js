bfa2562dcbf7b2e8ae54b684016e2018a863ee9d
jrlherm <lherm.jr@gmail.com>
2023-10-30 14:39:17 +0100

    integrated hamburger menu

--- script.js
+++ script.js
@@ -1,52 +1,12 @@
-// Tilt effect
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menu-toggle");
+  const menuItems = document.querySelectorAll(".menu-items a");
 
-$(document).on("mousemove", function (e) {
-  $(".light").css({
-    left: e.pageX - 300,
-    top: e.pageY - 300,
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      menuToggle.checked = false; // Uncheck the checkbox
+    });
   });
-});
-
-const element = $(".js-tilt-container");
-
-element.on("mousemove", function (e) {
-  const { left, top } = $(this).offset();
-  const cursPosX = e.pageX - left;
-  const cursPosY = e.pageY - top;
-  const cursFromCenterX = $(this).width() / 2 - cursPosX;
-  const cursFromCenterY = $(this).height() / 2 - cursPosY;
-
-  $(this).css(
-    "transform",
-    "perspective(500px) rotateX(" +
-      cursFromCenterY / 40 +
-      "deg) rotateY(" +
-      -(cursFromCenterX / 40) +
-      "deg) translateZ(10px)"
-  );
-
-  const invertedX =
-    Math.sign(cursFromCenterX) > 0
-      ? -Math.abs(cursFromCenterX)
-      : Math.abs(cursFromCenterX);
-
-  //Parallax transform on image
-  $(this)
-    .find(".js-perspective-neg")
-    .css(
-      "transform",
-      "translateY(" +
-        cursFromCenterY / 10 +
-        "px) translateX(" +
-        -(invertedX / 10) +
-        "px) scale(1.05)"
-    );
-
-  $(this).removeClass("leave");
-});
-
-element.on("mouseleave", function () {
-  $(this).addClass("leave");
 });
 
 // Loader
